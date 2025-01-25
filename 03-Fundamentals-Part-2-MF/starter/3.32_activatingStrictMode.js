a1969b547fb0aa71dafb2ee68f67b9e9d6390787
SAI KIRAN <saikirankemal@gmail.com>
2025-01-25 20:39:09 +0530

    added a file for strict mode

---
+++ 03-Fundamentals-Part-2-MF/starter/3.32_activatingStrictMode.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let hasDriversLicense = true;
+let passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive!");
+
+let interface = 10;
+let private = 20;

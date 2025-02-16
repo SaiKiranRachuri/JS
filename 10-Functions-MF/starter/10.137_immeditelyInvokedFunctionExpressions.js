bd38d3504361d12ec11d5dd6c8254734a09c3e05
SAI KIRAN <saikirankemal@gmail.com>
2025-02-16 20:07:42 +0530

    Made few changes

--- 10-Functions-MF/starter/10.137_immeditelyInvokedFunctionExpressions.js
+++ 10-Functions-MF/starter/10.137_immeditelyInvokedFunctionExpressions.js
@@ -1,19 +1,21 @@
 // IIFE : Function that needs only once
 
+const isPrivate = 22;
 (function () {
   console.log('This will never run again');
   const isPrivate = 23; // Encapsulation to prevent accidental damage
+  console.log(isPrivate); // 23
 })();
 
-// console.log(isPrivate);
+console.log(isPrivate);
 
 // Same using function expression
 (() => console.log('This will also never run again'))();
 
 // Block scope
 {
-  //   const isPrivate = 23;
+  const isPrivate = 23;
   var isPrivateTwo = 24;
 }
-// console.log(isPrivate);
-console.log(isPrivateTwo);
+console.log(isPrivate); // Not defined. As declared in a block
+console.log(isPrivateTwo); //Outputs 24.

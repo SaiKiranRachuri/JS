78cee6678736e1b695f37044e6c4d7672ed08150
Rachuri <SaiKiran.Rachuri2@thomsonreuters.com>
2025-04-09 15:55:01 +0530

    added files 14.230

--- 14-OOP/starter/14.230_challengeUsingClasses.js
+++ 14-OOP/starter/14.230_challengeUsingClasses.js
@@ -1,23 +1,84 @@
-const CarProto = {
-    init(make, speed){
+// Coding Challenge #2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+
+
+
+// Using classes
+class Car {
+    constructor(make, speed){
         this.make = make;
         this.speed = speed;
-    },
-
-    accelerate(speed){
-        this.speed += speed;
-        console.log(`${this.make} is going at a speed of ${this.speed}`);
-        
-    },
-
-    brake(speed){
-        this.speed -= speed;
-        console.log(`${this.make} is going at a speed of ${this.speed}`);
 
     }
+
+    // Methods are added to prototype property(PersonCl)
+    accelerate(){
+        this.speed += 5;
+        console.log(`${this.make} is moving with a speed of ${this.speed}`);
+    }
+
+    brake(){
+        this.speed -= 5;
+        console.log(`${this.make} is moving with a speed of ${this.speed}`);
+
+    }
+
+    get speedUS(){
+        this.speed /= 1.6;
+        console.log(`${this.make} is moving with a speed of ${this.speed} mi/h.`);
+
+
+    }
+
+    set speedUS(){
+        
+    }
+
+    
 }
 
-const ford = Object.create(CarProto);
-ford.init("Ford", 90);
+const ford = new Car("Ford", 90);
 ford.accelerate();
 ford.brake();
+
+ford.speedUS;
+
+
+
+
+
+
+// Using Object.create
+// const CarProto = {
+//     accelerate(){
+//         this.speed += 5;
+//         console.log(`${this.make} is going at a speed of ${this.speed}`);
+        
+//     },
+
+//     brake(speed){
+//         this.speed -= 5;
+//         console.log(`${this.make} is going at a speed of ${this.speed}`);
+
+//     },
+
+//     init(make, speed){
+//         this.make = make;
+//         this.speed = speed;
+//     },
+// }
+
+// const ford = Object.create(CarProto);
+// ford.init("Ford", 90);
+// ford.accelerate();
+// ford.brake();
